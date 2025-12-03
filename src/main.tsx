3f1e7c2b23558071e0a21ac54ab2bf7c14e9f7ae
GenSpark Developer <developer@genspark.ai>
2025-12-03 14:40:23 +0000

    Wrap App with StoryProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { StoryProvider } from './context/StoryContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoryProvider>
+      <App />
+    </StoryProvider>
   </React.StrictMode>,
 )
